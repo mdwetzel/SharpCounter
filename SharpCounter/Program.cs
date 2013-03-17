9cb51ee8a8cc7ecd5e3db23b926229518d172fee
Mark Wetzel <mark@markwetzel.com>
2013-03-17 14:44:29 -0700

    Add new count method and refactor.

--- SharpCounter/Program.cs
+++ SharpCounter/Program.cs
@@ -10,18 +10,32 @@ namespace SharpCounter
             Console.Write("Enter a string to count its words: ");
             string input = Console.ReadLine();
 
-            // I could just be lazy, but, nah.
+            int words = CountWordsByCharacter(input);
 
-            int words = 0;
-            for (int i = 0; i < input.Length; i++) {
-                if (input[i] == ' ' || i == input.Length - 1) {
-                    words++;
-                }
-            }
+            // words = CountWordWithSplit(input);
 
             Console.WriteLine("There are {0} words.", words);
 
             Console.ReadLine();
+        }
+
+        // I could just be lazy, but, nah.
+        private static int CountWordsByCharacter(string input)
+        {
+            int words = 0;
+            for (int i = 0; i < input.Length; i++) {
+                // i == input.Length - 1 = final word. 
+                if (input[i] == ' ' || i == input.Length - 1) {
+                    words++;
+                }
+            }
+            return words;
+        }
+
+        // The easy way.
+        private static int CountWordWithSplit(string input)
+        {
+            return input.Split(' ').Length;
         }
     }
 }
